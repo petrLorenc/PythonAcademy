a2db2b64da44fda180f94c0d17ee013a5e451778
Petr Lorenc <petr.lorenc@promethist.ai>
2020-09-22 07:15:55 +0200

    lesson 8: after-lesson

---
+++ lesson8/generate_text.py
@@ -0,0 +1,9 @@
+# find a book at https://www.gutenberg.org/ and save it as TXT file
+
+# load file a process it as unified as possible
+
+# create bigrams (don't know what it is - use google)
+
+# let the user give you word
+
+# choose next word according to bigrams and take care of boundary cases
---
+++ lesson8/using_argument_from_command_line.py
@@ -0,0 +1,12 @@
+# python using_argument_from_command_line.py argument1 argument2
+
+import sys
+
+
+def main():
+    print(sys.argv)
+
+
+if __name__ == '__main__':
+    main()
+
