63d01cefcd6a567b21d210ac3592b3ba386f269b
Petr Lorenc <petr.lorenc@promethist.ai>
2020-10-12 19:31:59 +0200

    lesson10: pre-lesson

---
+++ lesson10/my_module/data_hiding2.py
@@ -0,0 +1,13 @@
+_secret = 'Password123'
+print('Print is being executed')
+var1 = 100
+var2 = 110
+
+__all__ = ['var1', 'func']
+
+def __secret__():
+    print("My password is ***")
+
+def func(v1,v2):
+    result = v1 - v2
+    return result
